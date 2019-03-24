6523b120a71c911a0f0bfac2f1b13eff3151fdb8
Joey Robinson <theamazingness@gmail.com>
2019-03-23 23:19:00 -0400

    Title

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["babel-preset-env"]
+        }
+      }
+    }]
+  }
+};
